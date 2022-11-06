13b2709a6c645eafde27ffc6961889b708602116
HarshitS45 <20bcs054@iiitdwd.ac.in>
2022-11-06 15:59:07 +0530

    Digit Palindrome

--- Palindrome.c
+++ Palindrome.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 main()
 {
+	int again=0;
+	while(again==0)
+	{
 	int number,a,rev=0;
 	printf("\nEnter the number:");
 	scanf("%d",&number);
@@ -10,5 +13,29 @@ main()
 		rev=rev*10+(number%10);
 		number=number/10;
 	}
-    printf("Reverse of %d is %d",a,rev);
+	if(a==rev)
+    	printf("\nGiven number is palindrome\n\nReverse of the %d is %d",a,rev);
+    else
+    	printf("\nGiven number is not palindrome\n\nReverse of the %d is %d",a,rev);
+    printf("\n\nIf you want to find the reverse of another number press 0 otherwise press any digit:");
+    scanf("%d",&again);
+    }
 }
+/* Output
+Enter the number:12345
+
+Given number is not palindrome
+
+Reverse of the 12345 is 54321
+
+If you want to find the reverse of another number press 0 otherwise press any digit:0
+
+Enter the number:15051
+
+Given number is palindrome
+
+Reverse of the 15051 is 15051
+
+If you want to find the reverse of another number press 0 otherwise press any digit:1
+
+*/
