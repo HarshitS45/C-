1e8095cbc42a07ada6bc9367847ca45019b01e70
HarshitS45 <20bcs054@iiitdwd.ac.in>
2022-11-06 12:49:10 +0530

    Pyramid of stars

---
+++ pyramid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+	{
+		int i,j,k,l;
+		for(i=1,l=0;i<11&&l<11;i=i+2,l++)
+	{
+		for(k=55;k>l;k--)
+		printf(" ");
+	    for(j=0;j<i;j++)
+	   		printf("*");
+		printf("\n");
+	}}
+
