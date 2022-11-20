fb906dcdd6f276403dea869b9d750e4741457b2a
HarshitS45 <20bcs054@iiitdwd.ac.in>
2022-11-20 19:08:44 +0530

    check weather ear is leap year or not.

---
+++ Leap_year.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{
+	int year,again=0;
+	while(again<1)
+	{
+	printf("Enter the year:");
+	scanf("%d",&year);
+	if(year%4==0)
+		{
+		if(year%100==0)
+		{
+			if(year%400==0)
+			printf("\n%d is leap year\n",year);
+			else
+			printf("\n%d is not leap year\n",year);
+		}
+		else
+		printf("\n%d is leap year\n",year);
+		}
+		
+	else
+	printf("\n%d is not leap year\n",year);
+	printf("\nIf you want to enter again press 0\n");
+	scanf("\n%d",&again);
+	}
+}
