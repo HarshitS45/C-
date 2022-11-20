a1c6839eddf7cbc7753d7a9782063f5729694a8b
HarshitS45 <20bcs054@iiitdwd.ac.in>
2022-11-20 19:26:49 +0530

    Convert date to corresponding day.

--- dayCorrespondingToDate.c
+++ dayCorrespondingToDate.c
@@ -2,11 +2,9 @@
 #include<stdlib.h>
 int main()
 {
-    int date, year, month,c,t,m,century,a=0;
+    int date, year, month,c,t,m,century;
     system("color A5");
     printf("This program is only valid for 20th and 21st century\n ");
-    while(a==0)
-    {
     system("color B5");
     printf("\nEnter date:");
     scanf("%d",&date);
@@ -35,32 +33,24 @@ int main()
     m=4;
     else if(month==6)
     m=5;
-    else 
+    else
     m=6;
     t=(date+m+c+year+(year/4))%7;
-    if(date<32&&date>0&&month<13&&month>0&&year>-1&&year<100)
-	{
-		printf("\nThe day is ");
-    	system("color F5");
-   		if(t==1)
-		printf("sunday");    
-    	else if(t==2)
-		printf("monday");
-		else if(t==3)
-		printf("tuesday");
-	 	else if(t==4)
-		printf("wednesday");
-		else if(t==5)
-		printf("thursday");
-	 	else if(t==6)
-		printf("friday");
-	 	else 
-		printf("saturday");
-		printf(" corresponding to the date %d/%d/%d%d\n",date,month,century-1,year);
-	}
-	else
-		printf("\nInvalid input data");
-		printf("\nIf you want to calculate again press 0\n");
-		scanf("%d",&a);
-	}
+
+    system("color F5");
+        if(t==1)
+        printf("\nSunday");
+    else if(t==2)
+        printf("\nMonday");
+         else if(t==3)
+        printf("\nTuesday");
+         else if(t==4)
+        printf("\nWednesday");
+         else if(t==5)
+        printf("\nThursday");
+         else if(t==6)
+        printf("\nFriday");
+         else
+        printf("\nSaturday");
 }
+              
